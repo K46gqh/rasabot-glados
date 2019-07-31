877f052b49287e9ce1c943957b51b824db5a9139
K46gqh <46399372+K46gqh@users.noreply.github.com>
2019-07-31 14:43:02 +0800

    Update glados.py

--- glados.py
+++ glados.py
@@ -13,7 +13,7 @@ def train_nlu(data, configs, model_dir):
     
 def run_nlu():
 	interpreter = Interpreter.load('./models/nlu/default/trainedNlu')
-	print(interpreter.parse(u"I am planning my holiday to Lithuania. I wonder what is the weather out there."))
+	print(interpreter.parse(u"I am planning my holiday to Shanghai. What's the weather out there?"))
 
 if __name__ == '__main__':
 	train_nlu('./data/training_data.json', 'config_spacy.json', './models/nlu')
